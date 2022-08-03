fccaae8f9636e577a5f8d6f22ab4457a74c34ee6
Ibwedagama <wardana.wedagama@gmail.com>
2022-08-03 18:23:12 +0700

    refactor (feedback): refactor form data; adjust styling

--- components/Feedback/index.vue
+++ components/Feedback/index.vue
@@ -4,29 +4,77 @@
     <BaseModal :show="show" :full-screen="true" @close="closeFeedbackForm">
       <!-- Form Header -->
       <template #header>
-        <h1 class="font-roboto font-medium text-[21px] leading-[34px] text-green-700 px-6 py-2">
-          Seberapa Puas Anda dengan Portal Jabar
+        <h1 class="font-roboto font-medium text-[21px] leading-[34px] text-green-700 px-6 py-2 max-w-[568px]">
+          Berikan Masukkan untuk Perbaikan Portal Jabarprov
         </h1>
       </template>
       <!-- Form Body -->
-      <div class="p-6 overflow-y-auto">
-        <div class="flex flex-col mb-5">
-          <label class="text-sm leading-6 text-gray-800 mb-2">
-            Seberapa Puas Anda dengan Portal Jabar
+      <div class="p-6 overflow-y-auto max-w-[568px]">
+        <section class="flex flex-col mb-5">
+          <label class="text-sm leading-6 text-gray-800 mb-4">
+            Seberapa Puas Anda dengan Portal Jabarprov
           </label>
-          <div class="flex gap-7">
-            <JdsRadioButtonGroup
-              :items="ratingOptions"
-              placeholder-key="label"
-              name="radio-button-group"
-              orientation="horizontal"
-              @change="setFormRating"
-            />
+          <div class="feedback__emoticon w-full grid grid-cols-5 h-[65px] gap-3 mb-4">
+            <label
+              v-for="rating in ratingOptions"
+              :key="rating.label"
+              class="feedback__emoticon__container min-h-full cursor-pointer"
+            >
+              <input
+                v-model="form.rating"
+                type="radio"
+                :name="rating.label"
+                :value="rating.value"
+                class="feedback__emoticon__input absolute left-[-9999px]"
+              >
+              <div
+                class="feedback__emoticon__icon min-w-0 h-full bg-gray-50
+                flex flex-grow-0 rounded-lg items-center justify-center p-2 hover:bg-gray-200
+                transition-colors ease-brand duration-250"
+              >
+                <p class="text-xl md:text-4xl leading-none text-center">{{ rating.icon }}</p>
+              </div>
+            </label>
           </div>
-        </div>
-        <div class="flex flex-col mb-5">
-          <label id="compliments" class="text-sm leading-6 text-gray-800 mb-2">
-            Apa yang menurut Anda paling bermanfaat dari Portal Jabar?
+          <p v-if="!selectedRatingLabel" class="text-sm text-gray-700 text-center">
+            Pilih kepuasan Anda terlebih dahulu
+          </p>
+          <p v-else class="text-sm text-gray-700 text-center font-bold">
+            {{ selectedRatingLabel }}
+          </p>
+        </section>
+
+        <section class="flex flex-col mb-5">
+          <label class="text-sm leading-6 text-gray-800 mb-4">
+            Sektor apa yang paling mewakili Anda?
+          </label>
+          <JdsSelect
+            placeholder="Pilih Sektor"
+            :options="sectorOptions"
+            :value="form.sector"
+            class="feedback__sector__select"
+            @change="setFormSector"
+          />
+          <div v-if="form.sector === 'lainnya'" class="mt-4">
+            <textarea
+              id="sector"
+              v-model="userSector"
+              name="sector"
+              cols="30"
+              rows="3"
+              class="w-full bg-gray-50 rounded-lg p-2 text-sm mb-1 border border-gray-500"
+              placeholder="Masukkan sektor Anda berasal"
+              maxlength="1500"
+            />
+            <small class="text-gray-600 block">
+              {{ userSectorRemainingCharacters }} karakter tersisa
+            </small>
+          </div>
+        </section>
+
+        <section class="flex flex-col mb-5">
+          <label id="compliments" class="text-sm leading-6 text-gray-800 mb-4">
+            Apa yang bermanfaat bagi Anda dari Portal Jabarprov?
           </label>
           <textarea
             id="compliments"
@@ -34,17 +82,18 @@
             name="compliments"
             cols="30"
             rows="3"
-            class="bg-gray-50 rounded-lg p-2 text-sm mb-1"
+            class="bg-gray-50 rounded-lg p-2 text-sm mb-1 border border-gray-500"
             placeholder="Masukkan jawaban Anda di sini"
             maxlength="1500"
           />
           <small class="text-gray-600">
             {{ getRemainingCharacters('compliments') }} karakter tersisa
           </small>
-        </div>
-        <div class="flex flex-col mb-5">
-          <label id="criticism" class="text-sm leading-6 text-gray-800 mb-2">
-            Apa yang perlu diperbaiki dari Portal Jabar?
+        </section>
+
+        <section class="flex flex-col mb-5">
+          <label id="criticism" class="text-sm leading-6 text-gray-800 mb-4">
+            Apakah yang perlu diperbaiki dari Portal Jabarprov?
           </label>
           <textarea
             id="criticism"
@@ -52,17 +101,18 @@
             name="criticism"
             cols="30"
             rows="3"
-            class="bg-gray-50 rounded-lg p-2 text-sm mb-1"
+            class="bg-gray-50 rounded-lg p-2 text-sm mb-1 border border-gray-500"
             placeholder="Masukkan jawaban Anda di sini"
             maxlength="1500"
           />
           <small class="text-gray-600">
             {{ getRemainingCharacters('criticism') }} karakter tersisa
           </small>
-        </div>
-        <div class="flex flex-col mb-5">
-          <label id="suggestions" class="text-sm leading-6 text-gray-800 mb-2">
-            Apakah yang perlu Portal Jabar sediakan untuk kebutuhan pengguna?
+        </section>
+
+        <section class="flex flex-col mb-5">
+          <label id="suggestions" class="text-sm leading-6 text-gray-800 mb-4">
+            Menurut Anda apa yang perlu disediakan dari Portal Jabarprov?
           </label>
           <textarea
             id="suggestions"
@@ -70,14 +120,14 @@
             name="suggestions"
             cols="30"
             rows="3"
-            class="bg-gray-50 rounded-lg p-2 text-sm mb-1"
+            class="bg-gray-50 rounded-lg p-2 text-sm mb-1 border border-gray-500"
             placeholder="Masukkan jawaban Anda di sini"
             maxlength="1500"
           />
           <small class="text-gray-600">
             {{ getRemainingCharacters('suggestions') }} karakter tersisa
           </small>
-        </div>
+        </section>
       </div>
       <!-- Form Footer -->
       <template #footer>
@@ -137,15 +187,24 @@ export default {
         rating: null,
         compliments: '',
         criticism: '',
-        suggestions: ''
+        suggestions: '',
+        sector: ''
       },
       isLoading: false,
+      sectorOptions: [
+        { value: 'pemerintahan', label: 'Pemerintahan' },
+        { value: 'media', label: 'Media' },
+        { value: 'organisasi non profit', label: 'Organisasi Non Profit' },
+        { value: 'industri/bisnis', label: 'Industri/Bisnis' },
+        { value: 'lainnya', label: 'Lainnya (Tulis secara spesifik)' }
+      ],
+      userSector: '', // Custom sector provided by User
       ratingOptions: [
-        { label: '1', value: 1 },
-        { label: '2', value: 2 },
-        { label: '3', value: 3 },
-        { label: '4', value: 4 },
-        { label: '5', value: 5 }
+        { label: 'Sangat Tidak Puas', value: 1, icon: '😩' },
+        { label: 'Tidak Puas', value: 2, icon: '😐' },
+        { label: 'Cukup Puas', value: 3, icon: '😃' },
+        { label: 'Puas', value: 4, icon: '😍' },
+        { label: 'Sangat Puas', value: 5, icon: '🥰' }
       ],
       dialog: {
         open: false,
@@ -161,19 +220,30 @@ export default {
       const compliments = !!this.form.compliments.trim()
       const criticism = !!this.form.criticism.trim()
       const suggestions = !!this.form.suggestions.trim()
+      const sector = !!this.form.sector
 
-      return rating && compliments && criticism && suggestions
+      return rating && compliments && criticism && suggestions && sector
     },
     isValid () {
       return this.hasValue
+    },
+    selectedRatingLabel () {
+      if (this.form.rating) {
+        return this.ratingOptions.find(option => option.value === this.form.rating).label
+      }
+
+      return null
+    },
+    userSectorRemainingCharacters () {
+      return 1500 - this.userSector.length
     }
   },
   methods: {
     getRemainingCharacters (label) {
       return 1500 - this.form[label].length
     },
-    setFormRating (value) {
-      this.form.rating = value
+    setFormSector (value) {
+      this.form.sector = value
     },
     closeFeedbackForm () {
       this.$emit('close')
@@ -190,7 +260,8 @@ export default {
         rating: null,
         compliments: '',
         criticism: '',
-        suggestions: ''
+        suggestions: '',
+        sector: ''
       }
     },
     resetDialog () {
@@ -229,7 +300,8 @@ export default {
           rating: this.form.rating,
           compliments: this.form.compliments.trim(),
           criticism: this.form.criticism.trim(),
-          suggestions: this.form.suggestions.trim()
+          suggestions: this.form.suggestions.trim(),
+          sector: this.form.sector === 'lainnya' ? `lainnya - ${this.userSector}` : this.form.sector.trim()
         }
         try {
           this.isLoading = true
@@ -259,7 +331,31 @@ export default {
 
 <style scoped>
 textarea:focus {
+  border: 1px solid white;
   outline: none;
   box-shadow: inset 0px 0px 0px 1px #069550, inset 0px 0px 0px 2px #FFC800;
+}
+
+.feedback__emoticon__input[type=radio]:checked + .feedback__emoticon__icon {
+  @apply bg-green-600
+}
+</style>
+
+<style>
+/**
+  Override the default JDS Select styling
+*/
+.feedback__sector__select .jds-popover__activator,
+.feedback__sector__select .jds-select,
+.feedback__sector__select .jds-input-text {
+  @apply w-full
+}
+
+.feedback__sector__select .jds-input-text input{
+  @apply text-sm text-gray-700
+}
+
+.feedback__sector__select .jds-options__option-list {
+  @apply text-gray-700
 }
 </style>
