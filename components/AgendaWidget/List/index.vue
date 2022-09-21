bbb14bc8d6d1b52925fbf57aaa7470aa34530a7b
Naufal Ihsan Kusumayadhi <56071566+naufalihsank@users.noreply.github.com>
2022-09-21 16:49:38 +0700

    feat (agenda-widget): set infinite scroll to event list

--- components/AgendaWidget/List/index.vue
+++ components/AgendaWidget/List/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <slot name="header" />
-    <div class="bg-white flex flex-col overflow-hidden h-full xl:h-[380px]">
+    <div class="bg-white flex flex-col overflow-hidden h-[354px] sm:h-[390px] lg:h-[900px] xl:h-[350px]">
       <!--
         TODO: Show a placeholder if an error occur
       -->
@@ -12,19 +12,22 @@
         <AgendaWidgetEmptyState />
       </div>
       <div v-else class="flex flex-col flex-grow overflow-y-auto pt-1 pr-2 md:pr-4">
-        <AgendaWidgetListItem
-          v-for="event in events"
-          :id="event.id"
-          :key="event.id"
-          :title="event.title"
-          :date="event.date"
-          :category="event.category"
-          :type="event.type"
-          :url="event.url"
-          :address="event.address"
-          :start-hour="event.start_hour"
-          :end-hour="event.end_hour"
-        />
+        <InfiniteScroll :items="events" @refetch="getEvents">
+          <template #default="{ item }">
+            <AgendaWidgetListItem
+              :id="item.id"
+              :key="item.id"
+              :title="item.title"
+              :date="item.date"
+              :category="item.category"
+              :type="item.type"
+              :url="item.url"
+              :address="item.address"
+              :start-hour="item.start_hour"
+              :end-hour="item.end_hour"
+            />
+          </template>
+        </InfiniteScroll>
       </div>
       <Link v-if="hasEvents" link="/agenda-jawa-barat" class="flex justify-center py-5 border-t border-gray-100">
         <Button type="button" variant="tertiary-paddingless" tabindex="-1">
@@ -55,7 +58,8 @@ export default {
         pending: false,
         error: false
       },
-      perPage: 5
+      page: 1,
+      perPage: 10
     }
   },
   computed: {
@@ -66,6 +70,7 @@ export default {
   watch: {
     startDate () {
       this.events = []
+      this.page = 1
       this.getEvents()
     }
   },
@@ -74,18 +79,26 @@ export default {
   },
   methods: {
     async getEvents () {
-      this.fetchState.pending = true
+      if (this.page > this.totalPage) { return }
+
+      if (!this.events.length) {
+        this.fetchState.pending = true
+      }
 
       const params = {
         start_date: this.startDate,
         end_date: this.endDate,
-        per_page: this.perPage
+        per_page: this.perPage,
+        page: this.page
       }
 
       try {
         const response = await this.$axios.$get('/v1/public/events', { params })
 
-        this.events = response.data
+        this.events.push(...response.data)
+        this.totalPage = response.meta.total_page || 1
+        this.totalEvents = response.meta.total_count
+        this.page++
         this.fetchState.pending = false
       } catch (error) {
         this.fetchState.error = true
