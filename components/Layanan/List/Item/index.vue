7d48b3af46169ff75deea2a3cbe4ff5d62ad3878
naufalihsank <56071566+naufalihsank@users.noreply.github.com>
2022-09-28 13:53:52 +0700

    refactor (kependudukan-dan-tempat-tinggal): refactor and add loading skeleton

---
+++ components/Layanan/List/Item/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <NuxtLink
+    :to="`/layanan/kependudukan-dan-tempat-tinggal/${service.slug}`"
+    class="w-full flex flex-col gap-1 group bg-white px-4 py-3 rounded-lg
+    hover:bg-green-50 hover:cursor-pointer transition-colors ease-brand duration-250"
+  >
+    <LazyImg :src="service.logo" :alt="service.name" width="42" height="42" />
+    <h3 class="font-lato font-bold text-base text-blue-gray-700 leading-normal group-hover:text-green-700">
+      {{ service.name }}
+    </h3>
+    <p class="text-xs leading-6 text-blue-gray-400 group-hover:text-blue-gray-900 line-clamp-2">
+      {{ service.excerpt }}
+    </p>
+    <Button type="button" variant="tertiary-paddingless" class="font-lato self-start" tabindex="-1">
+      Selengkapnya
+      <Icon name="arrow-right" size="20px" />
+    </Button>
+  </NuxtLink>
+</template>
+
+<script>
+export default {
+  props: {
+    service: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
--- pages/layanan/kependudukan-dan-tempat-tinggal/index.vue
+++ pages/layanan/kependudukan-dan-tempat-tinggal/index.vue
@@ -14,32 +14,20 @@
             small
             :clear="false"
           />
-          <!-- TODO: Ganti Title di Counter Component -->
           <!-- TODO: add :loading="$fetchState.pending" -->
-          <FeaturedProgramCounter
+          <BaseListCounter
+            :title="'Total Layanan di OPD ini'"
+            :unit="'Layanan'"
             :counter="serviceLength"
             :loading="false"
             :last-update="meta.lastUpdate"
+            class="!w-full sm:!w-[294px]"
           />
-          <div v-for="service in serviceList" :key="service.id" class="grid grid-cols-3 gap-6">
-            <NuxtLink
-              :to="`/layanan/kependudukan-dan-tempat-tinggal/${service.slug}`"
-              class="w-full flex flex-col gap-1 group bg-white px-4 py-3 rounded-lg
-              hover:bg-green-50 hover:cursor-pointer transition-colors ease-brand duration-250"
-            >
-              <LazyImg :src="service.logo" :alt="service.name" width="42" height="42" />
-              <h3 class="font-lato font-bold text-base text-blue-gray-700 leading-normal group-hover:text-green-700">
-                {{ service.name }}
-              </h3>
-              <p class="text-xs leading-6 text-blue-gray-400 group-hover:text-blue-gray-900 line-clamp-2">
-                {{ service.excerpt }}
-              </p>
-              <Button type="button" variant="tertiary-paddingless" class="font-lato self-start" tabindex="-1">
-                Selengkapnya
-                <Icon name="arrow-right" size="20px" />
-              </Button>
-            </NuxtLink>
-          </div>
+          <!-- TODO: add :loading="$fetchState.pending" -->
+          <LayananList
+            :service-list="serviceList"
+            :loading="false"
+          />
         </div>
       </BaseContainer>
     </section>
@@ -74,6 +62,20 @@ export default {
       }
     }
   },
+  computed: {
+    serviceLength () {
+      return this.meta?.totalCount || 0
+    },
+    /**
+     * Check whether search should be active or not
+     * based on `searchValue` value
+     * @returns {Boolean}
+     */
+    isSearchActive () {
+      return this.searchValue.length > 3
+    }
+  },
+  // @TODO: get real data from backend
   // async fetch () {
   //   const params = {
   //     cat: 'disdukcapil'
@@ -90,9 +92,13 @@ export default {
   //     // silent error
   //   }
   // },
-  computed: {
-    serviceLength () {
-      return this.meta?.totalCount || 0
+  watch: {
+    searchValue: {
+      handler () {
+        if (this.isSearchActive) {
+          // @TODO: implement search
+        }
+      }
     }
   }
 }
