8641689b0529ee63744d426ccb0007ee31487af1
Naufal Ihsan Kusumayadhi <56071566+naufalihsank@users.noreply.github.com>
2022-10-12 16:15:12 +0700

    feat (image-preview): create base image preview component

---
+++ components/Base/ImagePreview/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <client-only>
+    <Portal to="modal">
+      <vue-easy-lightbox
+        :visible="show"
+        :imgs="images"
+        :index="index"
+        @hide="$emit('close')"
+      />
+    </Portal>
+  </client-only>
+</template>
+
+<script>
+export default {
+  props: {
+    show: {
+      type: Boolean,
+      default: false
+    },
+    images: {
+      type: Array,
+      default: () => []
+    },
+    index: {
+      type: [String, Number],
+      default: 0
+    }
+  }
+}
+</script>
