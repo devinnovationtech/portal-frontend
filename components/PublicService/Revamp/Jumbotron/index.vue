7e42527489ad9507cef93fbe71f5f129c6331c2d
Ida Bagus Wedagama <33661143+Ibwedagama@users.noreply.github.com>
2023-06-22 10:14:46 +0700

    chore: revamp public service layout (#477)
    * chore (public-service): add rewind-clock svg icon
    
    * chore (public-service): backup existing public service page
    
    * chore (public-service): add public service jumbotron revamp component
    
    * chore (public-service): add revamp jumbotron layout to public service page
    
    * style (public-service): improve responsive design on jumbotron and service-type
    
    * chore (public-service): public service page layout

---
+++ components/PublicService/Revamp/Jumbotron/index.vue
@@ -0,0 +1,132 @@
+<template>
+  <section
+    class="relative w-full bg-gray-800"
+    :style="jumbotronStyle"
+  >
+    <div class="overlay" />
+    <BaseContainer class="relative pt-24 pb-40 z-10">
+      <Breadcrumb :items="breadcrumbItems" :capitalize="false" class="mb-6" />
+
+      <div class="grid grid-cols-1 md:grid-cols-[80px,1fr,1fr] md:gap-x-4">
+        <img
+          :src="logo"
+          :alt="`Logo layanan ${title}`"
+          width="80"
+          height="80"
+          class="rounded-full object-cover object-center mb-3 md:row-span-3 md:mb-1 md:self-center"
+        >
+        <h1
+          class="font-lora font-bold text-[28px] leading-relaxed text-green-500 mb-2 md:col-span-2"
+        >
+          {{ title }}
+        </h1>
+        <span class="w-fit px-3 py-1 rounded-full bg-[#67676C] bg-opacity-40 text-white text-xs line-clamp-1 mb-4 md:col-span-2 md:mb-3">
+          {{ portalCategory }}
+        </span>
+        <div class="grid grid-cols-[auto_1fr] gap-x-[14px] md:flex md:col-span-2 md:mb-0">
+          <span class="text-xs text-white flex items-center">
+            <BaseIcon
+              icon="/icons/rewind-clock.svg"
+              :size="12"
+              fill-color="#fff"
+              class="mr-[10px]"
+            />
+            Terakhir diupdate {{ updatedAt }}
+          </span>
+          <span class="text-xs text-white flex items-center">
+            <BaseIcon
+              icon="/icons/category.svg"
+              :size="12"
+              fill-color="#fff"
+              class="mr-[10px]"
+            />
+            {{ category }}
+          </span>
+        </div>
+        <h2 class="font-lato text-sm leading-6 text-white line-clamp-5 mb-6 mt-1 md:mb-0 md:col-span-2 md:mt-5">
+          {{ subtitle }}
+        </h2>
+        <PublicServiceType
+          :type="serviceType"
+          class="mt-1 md:justify-self-end md:self-center md:mt-5"
+        />
+      </div>
+    </BaseContainer>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Jumbotron',
+  props: {
+    breadcrumbItems: {
+      type: Array,
+      required: true,
+      default: () => []
+    },
+    logo: {
+      type: String,
+      required: true,
+      default: ''
+    },
+    title: {
+      type: [String, Number],
+      required: true,
+      default: ''
+    },
+    subtitle: {
+      type: [String, Number],
+      required: false,
+      default: ''
+    },
+    portalCategory: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    category: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    updatedAt: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    backgroundImageUrl: {
+      type: String,
+      required: false,
+      default: '/images/jumbotron/default.webp'
+    },
+    serviceType: {
+      type: String,
+      required: false,
+      default: ''
+    }
+  },
+  computed: {
+    jumbotronStyle () {
+      return {
+        backgroundImage: `url('${this.backgroundImageUrl}')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center center',
+        backgroundRepeat: 'no-repeat'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.overlay {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  background: radial-gradient(100% 2589.12% at 0% 0%, rgba(0, 40, 19, 0.98) 0%, rgba(0, 32, 39, 0.9) 100%);
+}
+
+</style>
---
+++ pages/layanan/_category/_slug/index.backup.vue
@@ -0,0 +1,207 @@
+<template>
+  <main class="overflow-hidden">
+    <Jumbotron v-bind="jumbotron">
+      <template #breadcrumb>
+        <Breadcrumb :items="breadcrumbItems" :capitalize="false" class="mb-6" />
+      </template>
+
+      <template #suffix>
+        <PublicServiceType :type="type" />
+      </template>
+    </Jumbotron>
+
+    <section class="w-full bg-gray-200">
+      <BaseContainer class="relative -top-24 z-20">
+        <div v-if="!$fetchState.pending" class="min-h-screen p-3 md:p-4 lg:p-6 xl:py-8 xl:px-10 rounded-xl bg-white">
+          <PublicServiceHeader
+            :logo="logo"
+            :name="name"
+            :last-update="service.updated_at"
+          />
+          <PublicServiceMedia
+            :data="service.general_information"
+            class="mt-6"
+            @show-preview="showImagePreview"
+          />
+          <PublicServicePurpose
+            v-if="hasSection('purpose')"
+            :purpose="service.purpose"
+            class="mt-[80px]"
+          />
+          <PublicServiceFacility
+            v-if="hasSection('facility')"
+            :facility="service.facility"
+            class="mt-[80px]"
+          />
+          <PublicServiceRequirement
+            v-if="hasSection('requirement')"
+            :requirement="service.requirement"
+            class="mt-[80px]"
+          />
+          <PublicServiceTerm
+            v-if="hasSection('terms_of_service')"
+            :term="service.terms_of_service"
+            class="mt-[80px]"
+            @show-preview="showImagePreview"
+          />
+          <PublicServiceInfographic
+            v-if="hasSection('infographic')"
+            :images="infographics"
+            class="mt-[80px]"
+            @show-preview="showImagePreview"
+          />
+          <PublicServiceFAQ
+            v-if="hasSection('faq')"
+            :items="faq"
+            class="mt-[80px]"
+          />
+          <PublicServiceNews
+            v-if="newsList"
+            :service-name="alias"
+            :news="newsList"
+            :loading="$fetchState.pending"
+            class="mt-[80px]"
+          />
+        </div>
+
+        <!-- Skeleton -->
+        <PublicServiceSkeleton v-else />
+      </BaseContainer>
+    </section>
+
+    <!-- Image Preview -->
+    <BaseImagePreview
+      :show="showPreview"
+      :images="imagePreviewList"
+      :index="imagePreviewIndex"
+      @close="showPreview = false"
+    />
+  </main>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      jumbotron: {},
+      searchValue: null,
+      service: {},
+      newsList: [],
+      imagePreviewList: [],
+      showPreview: false,
+      imagePreviewIndex: 0
+    }
+  },
+  async fetch () {
+    const slug = this.$route.params.slug
+
+    try {
+      // get detail service data
+      const response = await this.$axios.get(`/v1/public/service-public/slug/${slug}`)
+      this.service = response.data.data
+
+      // get related news data
+      const params = {
+        q: this.alias,
+        per_page: 3,
+        sort_order: 'desc',
+        domain: ['news'],
+        fuzziness: false
+      }
+      const responseNews = await this.$axios.$get('/v1/search', { params })
+      this.newsList = responseNews.data
+
+      this.jumbotron = {
+        title: this.name,
+        subtitle: this.description,
+        backgroundImageUrl: '/images/jumbotron/default.webp'
+      }
+    } catch (e) {
+      // silent error
+      this.$nuxt.error({ statusCode: e.response.status, message: e.response?.data?.message })
+    }
+  },
+  fetchDelay: 1000,
+  computed: {
+    breadcrumbItems () {
+      return [
+        {
+          path: '/',
+          label: 'Beranda'
+        },
+        {
+          path: `/layanan/${this.category.path}?nama=${this.category.name}&kategori=${this.category.categoryValue}`,
+          label: this.category.name || '-'
+        },
+        {
+          path: this.$route.path,
+          label: this.name
+        }
+      ]
+    },
+    /**
+     * get category path based on current route
+     * @returns {String}
+     */
+    category () {
+      const path = this.$route.path.split('/')
+      const name = this.$route.query.nama
+      const categoryValue = this.$route.query.kategori
+
+      return {
+        name,
+        categoryValue,
+        path: path.length > 2 ? path[2] : '-'
+      }
+    },
+    type () {
+      return this.service.general_information
+        ? this.service.general_information.type
+        : '-'
+    },
+    logo () {
+      return this.service.general_information
+        ? this.service.general_information.logo
+        : '-'
+    },
+    name () {
+      return this.service.general_information
+        ? this.service.general_information.name
+        : '-'
+    },
+    description () {
+      return this.service.general_information
+        ? this.service.general_information.description
+        : '-'
+    },
+    alias () {
+      return this.service.general_information
+        ? this.service.general_information.alias
+        : '-'
+    },
+    infographics () {
+      return this.service.infographic
+        ? this.service.infographic.images
+        : []
+    },
+    faq () {
+      return this.service.faq
+        ? this.service.faq.items
+        : []
+    }
+  },
+  activated () {
+    this.$fetch()
+  },
+  methods: {
+    showImagePreview (image) {
+      this.imagePreviewList = image.images
+      this.imagePreviewIndex = image.index
+      this.showPreview = true
+    },
+    hasSection (sectionName) {
+      return this.service && Object.keys(this.service[sectionName]).length !== 0
+    }
+  }
+}
+</script>
