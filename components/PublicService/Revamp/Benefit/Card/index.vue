f636a4fabfe2768b9ce771c0efeaa8e12154d71a
Doohan Aditya Saputro <79241754+doohanas@users.noreply.github.com>
2023-06-23 10:46:13 +0700

    feat: revamp benefit section in public service (#478)
    * feat (benefit-card-component): create card component to show in benefit section
    
    * feat (benefit-section): revamp benefit section in public service feature
    
    * refactor (benefit-card): changge tag section into div
    
    * feat (revamp-list-component): create list component reusable
    
    * refactor (revamp-benefit-section): add revamp list component to handle item doesn't have image
    
    * refactor (list-component): change condition using truthy value

---
+++ components/PublicService/Revamp/Benefit/Card/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex flex-col w-full gap-4 md:flex-row">
+    <figure class="w-full h-[115px] bg-gray-200 rounded-[6px] overflow-hidden md:w-[125px]">
+      <LazyImg
+        :src="item.image.file_download_uri"
+        :alt="item.image.file_name"
+        width="265"
+        height="115"
+        class="w-full h-full object-cover object-center"
+      />
+    </figure>
+    <p class="w-full font-lato font-normal leading-[23px] text-sm text-blue-gray-900 text-left line-clamp-3 md:line-clamp-5">
+      {{ item.name }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      default: () => ({})
+    }
+  }
+}
+</script>
---
+++ components/PublicService/Revamp/Benefit/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="px-4 py-8 flex flex-col items-center gap-4">
+    <h2 class="font-lora font-bold text-[32px] text-center text-blue-gray-800 mb-4">
+      {{ benefit.title }}
+    </h2>
+    <ul class="flex flex-col items-center justify-center flex-wrap gap-4 w-full md:flex-row xl:w-[1280px]">
+      <li
+        v-for="(item, index) in benefit.items"
+        :key="index"
+        class="bg-[#F9FAFB] w-[288px] max-h-[240px] md:w-[316px] md:h-[147px] lg:w-[452px] xl:w-[462px] px-3 py-4"
+      >
+        <PublicServiceRevampBenefitCard v-if="isItemHasImage" :item="item" />
+        <PublicServiceRevampList v-else :item="item" />
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    benefit: {
+      type: Object,
+      required: true,
+      default: () => ({})
+    }
+  },
+  computed: {
+    isItemHasImage () {
+      return this.benefit.items.every(item => item.image.file_download_uri !== '')
+    }
+  }
+}
+</script>
