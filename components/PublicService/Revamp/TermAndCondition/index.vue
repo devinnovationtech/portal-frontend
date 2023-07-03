7529cfcc9633d6461311d96e2e0e425be77edfeb
Doohan Aditya Saputro <79241754+doohanas@users.noreply.github.com>
2023-07-03 10:18:06 +0700

    feat: slicing layout term and condition section (#487)
    * feat (term-and-condition-section): revamp term and condition section
    
    * refactor (term-and-condition): change alt value more general and meaningful

---
+++ components/PublicService/Revamp/TermAndCondition/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="flex flex-col items-center gap-4">
+    <h2 class="font-lora font-bold text-[32px] text-center text-blue-gray-800 mb-4 sm:max-w-xl">
+      {{ term.title }}
+    </h2>
+    <div v-if="term.cover.file_download_uri">
+      <div class="relative w-full group overflow-hidden rounded-xl">
+        <!-- Image Overlay -->
+        <div
+          class="opacity-0 inset-0 absolute flex items-center justify-center bg-[#00000080] group-hover:opacity-100 z-10 transition-opacity ease-brand duration-250 cursor-pointer"
+          @click="$emit('show-preview', { index: 0, images: [term.cover.file_download_uri] })"
+        >
+          <Icon src="/icons/zoom.svg" size="50px" />
+        </div>
+        <LazyImg
+          :src="term.cover.file_download_uri"
+          alt="Gambar Syarat dan Ketentuan"
+          class="w-full object-cover"
+        />
+      </div>
+    </div>
+    <ul class="grid grid-cols-1 justify-items-center gap-4 w-full sm:grid-cols-2">
+      <li
+        v-for="(item, index) in term.items"
+        :key="index"
+        class="bg-[#F9FAFB] px-3 py-4 rounded-xl md:w-[316px] 2xl:w-[450px]"
+      >
+        <PublicServiceRevampList :item="item" />
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    term: {
+      type: Object,
+      required: true,
+      default: () => ({})
+    }
+  }
+}
+</script>
