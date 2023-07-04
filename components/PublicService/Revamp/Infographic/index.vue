af569b73b1087e6ffd2d0fac3ae30c3e208d2e64
Doohan Aditya Saputro <79241754+doohanas@users.noreply.github.com>
2023-07-04 13:33:21 +0700

    feat (infographic-section): revamp infographics section in public service page (#491)

---
+++ components/PublicService/Revamp/Infographic/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <section class="lg:w-[900px]">
+    <client-only>
+      <swiper
+        v-show="swiperReady"
+        :options="swiperOptions"
+        :auto-update="true"
+        :auto-destroy="true"
+        :delete-instance-on-destroy="true"
+        :cleanup-styles-on-destroy="true"
+        class="infographic-swiper"
+        @ready="swiperReady = true"
+      >
+        <swiper-slide
+          v-for="(image, index) in images"
+          :key="`infographic-swiper-${index}`"
+          class="!w-[fit-content]"
+        >
+          <div class="relative w-[250px] h-[250px] md:w-[353px] md:h-[353px] rounded-xl overflow-hidden group">
+            <!-- Image Overlay -->
+            <div
+              class="opacity-0 inset-0 absolute flex items-center justify-center bg-[#00000080]
+              group-hover:opacity-100 z-10 transition-opacity ease-brand duration-250 cursor-pointer"
+              @click="handleImagePreview(index)"
+            >
+              <Icon src="/icons/zoom.svg" size="50px" />
+            </div>
+            <LazyImg
+              :src="image.file_download_uri"
+              width="250"
+              height="250"
+              class="object-cover w-full h-full"
+              :alt="`Infografis ${index}`"
+            />
+          </div>
+        </swiper-slide>
+      </swiper>
+
+      <!-- Render Placeholder -->
+      <template #placeholder>
+        <div class="flex justify-center w-[fit-content] overflow-x-hidden gap-6">
+          <div v-for="i in 3" :key="i" class="w-[250px] h-[250px] md:w-[353px] md:h-[353px] rounded-xl bg-gray-300 animate-pulse" />
+        </div>
+      </template>
+    </client-only>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    images: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data () {
+    return {
+      swiperReady: false,
+      swiperOptions: Object.freeze({
+        slidesPerView: 'auto',
+        spaceBetween: 24,
+        mousewheel: true,
+        passiveListeners: true,
+        centerInsufficientSlides: true
+      })
+    }
+  },
+  methods: {
+    handleImagePreview (index) {
+      const imageList = this.images.map(image => image.file_download_uri)
+      this.$emit('show-preview', { index, images: imageList })
+    }
+  }
+}
+</script>
