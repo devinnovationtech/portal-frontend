884e969dc8896c1ba612217485c9bfed3564d9c6
Doohan Aditya Saputro <79241754+doohanas@users.noreply.github.com>
2023-07-12 09:47:10 +0700

    refactor: add padding, new conditional in li items and remove item-start (#517)
    * refactor (term-and-condition): add padding in x axis in wrapper section
    
    * refactor (term-and-condition): remove item-start in ul element
    
    * refactor (term-and-condition): create conditional when item more than equal to two items

--- components/PublicService/Revamp/TermAndCondition/index.vue
+++ components/PublicService/Revamp/TermAndCondition/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="py-8 flex flex-col items-center gap-8">
+  <section class="py-8 px-6 xl:px-[64px] flex flex-col items-center gap-8">
     <h2 class="font-lora font-bold text-[32px] text-center text-blue-gray-800 mb-4 sm:max-w-xl">
       {{ title }}
     </h2>
@@ -21,7 +21,12 @@
         />
       </div>
     </div>
-    <ul class="grid grid-cols-1 justify-items-center items-start gap-4 w-full sm:grid-cols-2">
+    <ul
+      :class="{
+        'grid grid-cols-1 justify-items-center gap-4 w-full': true,
+        'sm:grid-cols-2': items.length >= 2,
+      }"
+    >
       <li
         v-for="(item, index) in items"
         :key="index"
