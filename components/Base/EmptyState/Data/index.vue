7eee9c859159cdf830902ca259070066c4de71b8
Doohan Aditya Saputro <79241754+doohanas@users.noreply.github.com>
2023-07-28 13:20:07 +0700

    feat: create empty state when the data is not available in the public service category (#526)
    * chore: add empty data icon
    
    * feat (empty-state): to show the user message of the the data is not available or not provide
    
    * feat (empty-state): set empty state data compnent in public service category
    
    * refactor (empty-state): create title props and set title value in public service category

---
+++ components/Base/EmptyState/Data/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="w-full flex flex-col items-center justify-center bg-white p-6 min-h-[400px]">
+    <img
+      src="/icons/empty-data.svg"
+      alt="Pencarian tidak ditemukan"
+      width="120px"
+      height="120px"
+      class="mb-4"
+    >
+    <p class="font-roboto text-[21px] leading-[34px] font-bold text-gray-900 text-center mb-0.5 max-w-[617px]">
+      {{ title }}
+    </p>
+    <p class="font-lato text-center text-[12px] leading-[19px] text-gray-700 max-w-[483px]">
+      {{ errorMessage }}
+    </p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'BaseEmptyStateData',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    errorMessage: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
--- components/PublicService/Category/index.vue
+++ components/PublicService/Category/index.vue
@@ -4,9 +4,14 @@
       <div v-for="index in 3" :key="index" class="w-full h-[166px] bg-gray-200 rounded-lg animate-pulse" />
     </div>
     <BaseEmptyState
-      v-else-if="serviceList.length === 0"
+      v-else-if="!!searchValue"
       error-message="Mohon maaf kami tidak dapat menemukan nama layanan yang cocok dengan"
       :search-value="searchValue"
+    />
+    <BaseEmptyStateData
+      v-else-if="serviceList.length === 0"
+      title="Maaf, layanan belum tersedia"
+      error-message="Kami memahami bahwa Anda ingin menemukan layanan tertentu, namun saat ini layanan tersebut belum tersedia di platform kami."
     />
     <div v-else class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
       <PublicServiceCategoryItem
